acd509677cfa7519f446bc245c81f882748fa6e6
asilatakarakoc <80043934+asilatakarakoc@users.noreply.github.com>
2025-08-10 15:27:08 +0300

    Initial Commit

---
+++ StayAwake/StayAwakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayAwakeApp.swift
+//  StayAwake
+//
+//  Created by Asil Ata Karakoç on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct StayAwakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StayAwakeTests/StayAwakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  StayAwakeTests.swift
+//  StayAwakeTests
+//
+//  Created by Asil Ata Karakoç on 10.08.2025.
+//
+
+import Testing
+@testable import StayAwake
+
+struct StayAwakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
